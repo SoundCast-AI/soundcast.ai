1351400a1af803b3ef3d10765493d90fe6560ef5
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-11-02 12:39:28 +0530

    Added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "@fontsource/inter";
+import React from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head></Head>
+        <GoogleAnalytics gaId="G-68G8T7K84E" />
         <body>
           <Main />
           <NextScript />
